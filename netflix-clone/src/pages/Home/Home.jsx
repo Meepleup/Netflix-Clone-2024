b03e1f5b85e4e4f7c2b188cbdbf76be2aebe67dd
Samual Molla <sm@MeepleUp.local>
2025-12-01 20:26:56 +0100

    header and footer

---
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/Header/Header.jsx'
+import Footer from '../../components/Footer/footer.jsx'
+
+
+function Home() {
+  return (
+    <div>
+      <Header/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
