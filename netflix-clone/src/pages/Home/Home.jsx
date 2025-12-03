7fad801e194e1092367fe202d2c4c3a01caf0c6c
Samual Molla <sm@MeepleUp.local>
2025-12-03 19:26:36 +0100

    Banner

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header/Header.jsx'
+import Banner from '../../components/Banner/Banner.jsx'
 import Footer from '../../components/Footer/footer.jsx'
-
 
 function Home() {
   return (
     <div>
       <Header/>
+      <Banner/>
       <Footer/>
     </div>
   )
